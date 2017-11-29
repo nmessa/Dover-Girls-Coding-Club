e22c0a92fd80acfd08fe2662ec579cbacd29316a
nmessa <nmessa@sau16.org>
2017-11-29 07:35:23 -0500

    Add files via upload

---
+++ Code for 10.17.2017/example1.py
@@ -0,0 +1,87 @@
+#variables and data types
+#variable can be numbers, strings, or boolean values
+#numbers can be integers or floating point numbers
+
+#variable names can have letters, numbers, or underscore
+#variable names must start with letter or underscore
+#variable names should have meaning
+
+#What data types are each of these variables
+##number1 = 42
+##number2 = 3.14
+##name = 'Mary'
+##name2 = "Josephine"
+
+#What is different about name and name2?
+
+#Operators
+#Arithmetic operators
+##number = 13 + 7
+##print(number)
+##number = 13 - 7
+##print(number)
+##number = 13 * 7
+##print(number)
+##number = 13 / 7
+##print(number)
+##number = 13 // 7
+##print(number)
+##number = 13 % 7
+##print(number)
+##number = 13 ** 7
+##print(number)
+
+#Relational operators
+##answer = 10 > 4
+##print(answer)
+##answer = 10 < 4
+##print(answer)
+##answer = 10 == 4
+##print(answer)
+##answer = 10 != 4
+##print(answer)
+##answer = 10 >= 4
+##print(answer)
+##answer = 10 <= 4
+##print(answer)
+
+#Logical
+##answer = True and True
+##print(answer)
+##answer = True and False
+##print(answer)
+##answer = True or False
+##print(answer)
+##answer = False or False
+##print(answer)
+##answer = not False
+##print(answer)
+
+#what does the following print?
+##answer = 10 > 4 and 3 < 7
+##print(answer)
+##answer = 10 < 4 or 3 < 7
+##print(answer)
+##answer = 10 < 4 or 3 > 7
+##print(answer)
+
+
+#Your turn
+#Write a program that prints an address label of the following format:
+#       Jane Austin
+#       13 Mockingbird Lane
+#       Dover, NH   03820
+#using variables for name, address, city, state, and zipcode
+
+
+
+
+
+
+#calculate the following values sum, difference, product, quotient, and x^y given
+##x = 123.54
+##y = 12.9
+
+
+
+
