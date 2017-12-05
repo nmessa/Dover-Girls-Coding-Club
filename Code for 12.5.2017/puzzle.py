1dd1061569b084ca4b553b27f95de5cdc587eb47
nmessa <nmessa@sau16.org>
2017-12-05 16:13:22 -0500

    Add files via upload

---
+++ Code for 12.5.2017/puzzle.py
@@ -0,0 +1,31 @@
+#Problem 1
+#Print 151015 using only variables a and b
+a = 10
+b = "15"
+
+
+
+#Problem 2
+#Print 26 using only variables a and b
+
+a = 10
+b = "16"
+
+
+
+#Problem 3
+#Print 37 using only variables a and b
+a = 1
+b = "2"
+
+
+
+
+#Problem 4
+#Print 90210 using only variables a and b
+a = 90
+b = "10"
+c = "11"
+d = 0
+
+
